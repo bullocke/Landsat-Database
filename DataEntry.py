fdc319dd391749ed7c9532577e65ebeaf4039ef6
Eric  Bullock <bullocke@geo.bu.edu>
2014-11-21 10:35:45 -0500

    Fixed format to be centralized from database instead of shapefile

--- DataEntry.py
+++ DataEntry.py
@@ -4,138 +4,58 @@
 # In[1]:
 
 import sqlite3
-from collections import OrderedDict
 import fiona
 import json
 from json import *
 import gdal
 from osgeo import ogr
 import os
+import collections
 
 
-# In[35]:
+# In[2]:
 
 #Retrieve variables
 modeltype = raw_input("YATSM or CCDC? ")
+model = modeltype.upper()
 projectname = raw_input("Enter project name: ")
 pathrow1 = raw_input("Enter Path Row PPPRRR: ")
 username = raw_input("Enter the user who processed the scene: ")
-if modeltype == "YATSM" or modeltype == "yatsm":
+if model == "YATSM":
     locationname = raw_input("Enter location on server of parameter file: ")
-elif modeltype == "CCDC" or modeltype == "ccdc":
+elif model == "CCDC":
     minRMSE = raw_input("Enter the minimum RMSE (If unknown just press [ENTER]): ")
     changeprob = raw_input("Enter the probability of change value (If unknown just press [ENTER]): ")
     noiseprob = raw_input("Enter the noise probability value (If unknown just press [ENTER]): ")
-    consecChange = raw_input("Enter the number of consecutive observations for a model fit (If unknown just press [ENTER]): ")
+    consecChange = raw_input("Enter the number of consecutive observations for a confirmed change (If unknown just press [ENTER]: ")
     coefs = raw_input("Enter the number of coefficients used (If unknown just press [ENTER]): ")
     locationname = raw_input("Enter the location of the image stack on the server: ")            
 else:
     print "Please try the script again"
     exit()
     
-if modeltype == "YATSM" or modeltype == "yatsm":
-    print 'Your scene characteristics are:\nModel Run: %s\nPathRow: %s\nParameter Location: %s\nProcesing User: %s' % (modeltype, pathrow1, locationname, username)
+if model == "YATSM":
+    print '\n\nYour scene characteristics are:\nModel Run: %s\nPathRow: %s\nParameter Location: %s\nProcesing User: %s' % (modeltype, pathrow1, locationname, username)
+    answer = raw_input("Is this correct? y/n ")
 else:
-    print '\n\nYour scene characteristics are:\nModel Run: %s\nPathRow: %s\nMinimum RMSE: %s\nChange Probability: %s\nNoise Probability: %s\nCoefficients: %s\nStack Location: %s\n' %(modeltype, pathrow1, minRMSE, changeprob, noiseprob, coefs, locationname)
-
-answer = raw_input("Is this correct? y/n ")
+    print '\n\nYour scene characteristics are:\nModel Run: %s\nPathRow: %s\nMinimum RMSE: %s\nChange Probability: %s\nNoise Probability: %s\nConsecutive Observations: %s\nCoefficients: %s\nStack Location: %s\nUsername: %s\n' %(modeltype, pathrow1, minRMSE, changeprob, noiseprob, consecChange, coefs, locationname, username)
+    answer = raw_input("Is this correct? y/n ")
     
 if answer == 'n':
     exit()
 
 
-# In[37]:
-
-#Determine the styling
-strokecolor = '#000000'
-opaquenumb = 1
-if modeltype == 'yatsm' or modeltype == 'YATSM':
-    fillcolor = '#C00000'
-else:
-    fillcolor = '#0000CC'
-    
-
-
-# In[38]:
-
-#Prepare the variables for the YATSM shapefile
-pathrow2 = pathrow1.lstrip("0")
-_model = modeltype
-_pr = int(pathrow2)
-_project = projectname
-_author = username
-_location = locationname
-_fill = fillcolor
-_stroke = strokecolor
-_opaque = opaquenumb
-
-_properties = OrderedDict([
-    ['fill', 'str'],
-    ['stroke', 'str'],
-    ['fill-opacity', 'int'],
-    ['Model', 'str'],
-    ['WRS2', 'int'],
-    ['Project', 'str'],
-    ['Author', 'str'],
-    ['Location', 'str']
-])
-
-
-# In[47]:
-
-#Prepare the schema
-_schema = {'geometry': 'Polygon',
-           'properties': _properties,
-           }
-
-
-# In[40]:
-
-#Match PathRow with WRS2 shapefile
-source = fiona.open('/usr3/graduate/bullocke/bin/Database/HelperFiles/wrs2_descending.shp')
-for rec in source:
-    if rec['properties']['WRSPR'] == _pr:
-        break
-
-
-# In[53]:
-
-#Update the database shapefile for YATSM
-with fiona.open('/usr3/graduate/bullocke/bin/Database/HelperFiles/PRmap.shp') as f:
-    elem = next(f)
-    elem['geometry'] = rec['geometry']
-    elem['properties'] = {
-        'fill': _fill,
-        'stroke': _stroke,
-        'Model': _model,
-        'WRS2': _pr,
-        'Project': _project,
-        'Author': _author,
-        'Location': _location,
-}
-with fiona.open('/usr3/graduate/bullocke/bin/Database/HelperFiles/PRmap.shp', 'a') as f:
-    f.write(elem)
-
-
-# In[54]:
-
-#Convert shapefile to .GeoJSON to be used on GitHub using Leaflet.js
-print "Updating GeoJSON file"
-os.remove("PRmap.geojson")
-os.system("ogr2ogr -f GeoJSON -t_srs crs:84 PRmap.geojson /usr3/graduate/bullocke/bin/Database/HelperFiles/PRmap.shp")
-os.system("chmod g+rwx PRmap.geojson")
-
-# In[13]:
+# In[3]:
 
 #Start process of updating database
-print 'Updating database'
+print 'Updating database...'
 conn = sqlite3.connect('SceneDatabase.db')
 c = conn.cursor()
 
 
-# In[14]:
+# In[4]:
 
-#Define data entry functions
+#Define the functions for entering data
 def yatsmDataEntry():
     c.execute("INSERT INTO yatsm(Project, PathRow, User, Location) VALUES(?,?,?,?)",
               (projectname, pathrow1, username, locationname))
@@ -145,42 +65,151 @@ def ccdcDataEntry():
     c.execute("INSERT INTO ccdc(Project, PathRow, User, MinRMSE, ChangeProb, NoiseProb, ConsecChange, Coef, LocationCCDC) VALUES(?,?,?,?,?,?,?,?,?)",
               (projectname, pathrow1, username, minRMSE, changeprob, noiseprob, consecChange, coefs, locationname))
     conn.commit()
+    
+def bothDataEntry():
+    c.execute("INSERT INTO both(Project, PathRow, User, Location) VALUES(?,?,?,?)",
+              (projectname, pathrow1, username, locationname))
+    conn.commit()
 
 
-# In[22]:
+# In[5]:
 
-if modeltype == "YATSM" or modeltype == "yatsm":
+#Update tables
+if model == "YATSM":
     yatsmDataEntry()
-if modeltype == "CCDC" or modeltype == "ccdc":
+if model == "CCDC":
     ccdcDataEntry()
+    
+bothDataEntry()
 
 
-# In[47]:
+# In[6]:
 
-##Show table in python if desired
-#c.execute("Select * from yatsm")
-#table=c.fetchall()
-#for x in table:
-#    print x
+#Exporting 'both' table to .csv using Pandas
+print "Updating .CSV files..."
+import pandas.io.sql as panda
+con = sqlite3.connect('SceneDatabase.db')
+table = panda.read_sql('select * from both', con)
+table.to_csv('SceneList.csv')
+table2 = panda.read_sql('select * from yatsm', con)
+table2.to_csv('YATSM_Scenes.csv')
+table3 = panda.read_sql('select * from ccdc', con)
+table3.to_csv('CCDC_Scenes.csv')
 
 
-# In[24]:
+# In[7]:
 
-#Update CSV File for yatsm
+#Make the template for all but the last input of GeoJSON
+template =     ''' { "type" : "Feature",
+        "properties" : { "fill" : "%s", "stroke" : "%s", "fill-opacity" : "0.8", "WRS2" : "%s", "Project" : "%s", "Author" : "%s", "Location" : "%s"}, "geometry" : %s},
+    '''
+
+
+# In[8]:
+
+#Make the template for the last input of GeoJSON
+template2 =     ''' { "type" : "Feature",
+        "properties" : { "fill" : "%s", "stroke" : "%s", "fill-opacity" : "0.8", "WRS2" : "%s", "Project" : "%s", "Author" : "%s", "Location" : "%s"}, "geometry" : %s}
+    '''
+
+
+# In[9]:
+
+#Setup the beginning of the GeoJSON file
+output =     ''' {"type": "FeatureCollection",
+"crs": { "type": "name", "properties": { "name": "urn:ogc:def:crs:OGC:1.3:CRS84" } },
+"features": [
+    ''' \
+
+
+# In[10]:
+
+
+linecolor = '#000000'
+# In[12]:
+
+#Loop over each row in the CSV and plug into templates
+#Note: There has to be a better way to do this...
 import csv
-print 'Updating CSV'
-if modeltype == "YATSM" or modeltype == "yatsm":
-    c.execute('select * from yatsm')
-    with open('YATSM_Scenes.csv', 'wb') as fout:
-        writer = csv.writer(fout)
-        writer.writerow(['ID','Project','Path Row', 'User', 'Parameter File Location']) # heading row
-        writer.writerows(c.fetchall())
-if modeltype == "CCDC" or modeltype == "ccdc":
-    c.execute('select * from ccdc')
-    with open('CCDC_Scenes.csv', 'wb') as fout:
-        writer = csv.writer(fout)
-        writer.writerow(['ID','Project','Path Row', 'User', 'MinRMSE', 'Change Probability', 'Noise Probability','Consecutive Observations for Change', '# Coefficients', 'Stack Location']) # heading row
-        writer.writerows(c.fetchall())
+iter = 0
+print "Updating GeoJSON file"
+f = open('SceneList.csv')
+rawData = csv.reader(f.readlines())
+t = len(open('SceneList.csv').readlines())
+#print t
+for row in rawData:
+    iter += 1
+    if row[2].upper() == 'CMS_MEXICO' or projectname.upper() =='CMS MEXICO':
+        fillcolor = '#fb8072'
+    elif row[2].upper() == 'ACRE':
+        fillcolor = '#8dd3c7'
+    elif row[2].upper() == 'AMAZON':
+        fillcolor = '#ffffb3'
+    elif row[2].upper() == 'GUANGZHOU':
+        fillcolor = '#bebada'
+    elif row[2].upper() == 'BOSTON':
+        fillcolor = '#80b1d3'
+    elif row[2].upper() == 'CMS':
+        fillcolor = '#fdb462'
+    elif row[2].upper() == 'IDS':
+        fillcolor = '#b3de69'
+    elif row[2].upper() == 'LCMS':
+        fillcolor = '#fccde5'
+    elif row[2].upper() == 'VIETNAM':
+        fillcolor = '#d9d9d9'
+    elif row[2].upper() == 'CAMBODIA':
+        fillcolor = '#bc80bd'
+    elif row[2].upper() == 'FUSION':
+        fillcolor = '#ccebc5'
+    else:
+        fillcolor = '#737373'
+    if iter >= 2 and iter != t:
+        source = fiona.open('/usr3/graduate/bullocke/bin/Database/HelperFiles/wrs2_descending.shp')
+        for rec in source:
+            if rec['properties']['WRSPR'] == int(row[3]):
+                break
+        PR = rec['geometry']
+        PR2 = str(PR).replace('(','[').replace(')',']').replace('\'','\"')
+        WRS2 = row[2]
+        Project = row[3]
+        Author = row[4]
+        Location = row[5]
+        output += template % (fillcolor, linecolor, row[3], row[2], row[4], row[5], PR2)
+    if iter == t:
+        source = fiona.open('/usr3/graduate/bullocke/bin/testing/database2/Database/HelperFiles/wrs2_descending.shp')
+        for rec in source:
+            if rec['properties']['WRSPR'] == int(row[3]):
+                break
+        PR = rec['geometry']
+        PR2 = str(PR).replace('(','[').replace(')',']').replace('\'','\"')
+        WRS2 = row[2]
+        Project = row[3]
+        Author = row[4]
+        Location = row[5]
+        output += template2 % (fillcolor, linecolor, row[3], row[2], row[4], row[5], PR2)
+
+
+# In[13]:
+
+#Add the tail to the output file
+output +=     '''     ]
+}
+    '''
+
+
+# In[147]:
+
+#Open the GeoJSON and update
+
+outFileHandle = open("PRmap.geojson", "w")
+outFileHandle.write(output)
+outFileHandle.close()
+
+
+print "Done! Thank you!"
+# In[ ]:
+
+
 
 
 # In[ ]:
